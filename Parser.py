081d4c0fd6897d88935b1192508d797f134e044f
CharllyP <charlly84000@hotmail.fr>
2020-12-01 11:13:45 +0100

    ajout des auteurs et des references et la gestion des cas -t/-x ou autres

--- Parser.py
+++ Parser.py
@@ -8,36 +8,62 @@ def parser():
     os.system("rm -r ParsedPapers")
     os.system("mkdir ParsedPapers")
 
-    dossierSource = sys.argv[1]
+
+    dossierSource = sys.argv[2]
     listePDF = glob.glob(dossierSource+"*.pdf")
+    option = sys.argv[1]
 
-    for i in listePDF:
-        toConvert = i
-        converted = i + ".txt"
-        os.system("touch "+ converted)
-        command = "pdftotext " + toConvert + " " + converted
-        os.system(command)
-        fr = open(converted, "r+")
-        
-        # Nom
-        iStripped = i.strip(dossierSource)
-        parsed = "ParsedPapers/" + iStripped + "Parsed.txt"
-        fw = open(parsed, 'w+')
-        fw.write(iStripped+ "\n")
-        
-        # Titre
-        titre = fr.readline()
-        fw.write(titre)
+    if(option == "-t"):
+        for i in listePDF:
+            toConvert = i
+            converted = i + ".txt"
+            os.system("touch "+ converted)
+            command = "pdftotext " + toConvert + " " + converted
+            os.system(command)
+            fr = open(converted, "r+")
 
-        # Abstract
-        abstract = fr.read()
-        debut = (abstract.find("Abstract"))
-        fin = (abstract.find("\n\n", debut))
-        substring = abstract[debut:fin]
-        fw.write(substring)
-        fw.close()
+            # Nom
+            iStripped = i.strip(dossierSource)
+            parsed = "ParsedPapers/" + iStripped + "Parsed.txt"
+            fw = open(parsed, 'w+')
+            fw.write(iStripped+ "\n")
+
+            # Titre
+            titre = fr.readline()
+            fw.write(titre)
+
+            read = fr.read()
+
+            # Authors
+            substring = read.lstrip(titre)
+            substring = substring.strip()
+            fin = (read.find("Abstract"))
+            substring = read[:fin]
+            fw.write(substring)
+
+
+            # Abstract
+            debut = (read.find("Abstract"))
+            fin = (read.find("\n\n", debut))
+            substring = read[debut:fin]
+            fw.write(substring)
+
+            # References
+            debut = (read.find("References"))
+            substring = read[debut:]
+            fw.write("\n"+substring)
+
+            fw.close()
+
+    elif(option == "-x"):
+        print("xml")
+    else:
+        print("Option inconnue !")
 
 def main():
-    parser()
+    if(len(sys.argv) != 3):
+        print("Veuillez suivre cette syntaxe: 'python3 Parser.py -(t ou x) RepertoireSource/")
+    else:
+        parser()
 
 main()
